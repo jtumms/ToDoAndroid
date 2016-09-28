bffc614a5c866634011e87baa0bcce4534f4e6f2
jtumms <john.tumminelli@tummsmedia.com>
2016-09-28 11:12:52 -0400

    Finished code

--- app/src/main/java/com/example/johntumminelli/todoandroid/MainActivity.java
+++ app/src/main/java/com/example/johntumminelli/todoandroid/MainActivity.java
@@ -2,12 +2,61 @@ package com.example.johntumminelli.todoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener, AdapterView.OnItemClickListener {
+    ListView list;
+    EditText text;
+    Button addButton;
+
+    ArrayAdapter<ToDoItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+        text = (EditText) findViewById(R.id.editText);
+        addButton = (Button) findViewById(R.id.button);
+
+        items = new ArrayAdapter<ToDoItem>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(items);
+
+        addButton.setOnClickListener(this);
+
+        list.setOnItemLongClickListener(this);
+        list.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String t = text.getText().toString();
+        if (!t.isEmpty()){
+            ToDoItem item = new ToDoItem(t, false);
+            items.add(item);
+            text.setText("");
+        }
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        ToDoItem item = items.getItem(position);
+        items.remove(item);
+        return true;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ToDoItem item = items.getItem(position);
+        item.isDone = !item.isDone;
+        items.notifyDataSetChanged();
+
+
     }
 }
---
+++ app/src/main/java/com/example/johntumminelli/todoandroid/ToDoItem.java
@@ -0,0 +1,25 @@
+package com.example.johntumminelli.todoandroid;
+
+/**
+ * Created by john.tumminelli on 9/28/16.
+ */
+
+public class ToDoItem {
+    String text;
+    boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+    }
+
+    @Override
+    public String toString() {
+        String checkbox = "[ ]";
+        if (isDone) {
+            checkbox = "[x]";
+        }
+        return String.format("%s  %s", checkbox, text);
+
+    }
+}
